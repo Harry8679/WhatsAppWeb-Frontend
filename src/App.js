34425da6c40451a99b9c160a7089a1795fd5577b
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-24 12:05:24 +0200

    Tailwindcss installed and setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ArrowIcon from './svg/Arrow';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the Frontend</h1>
+      <h1 className='text-red-600'>Welcome to the Frontend</h1>
     </div>
   );
 }
