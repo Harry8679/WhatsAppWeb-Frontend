c94285109a2da33cb028f7b944c27f76df7d0803
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 20:54:51 +0100

    Email validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,5 +4,7 @@ export const signUpSchema = Yup.object({
     name: Yup.string().required('Full name is required')
         .matches(/^[a-zA-Z_ ]*$/, 'No special characters allowed.')
         .min(2, 'Name must be between 2 and 16 characters.')
-        .max(16, 'Name must be between 2 and 16 characters.')
+        .max(16, 'Name must be between 2 and 16 characters.'),
+    email: Yup.string().required('Email address is required')
+        .email('Invalid email address')
 });
