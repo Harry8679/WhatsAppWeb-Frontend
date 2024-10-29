7b480b9ad413d0aa3bcb72ce38de04cee68e4082
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 19:19:46 +0100

    Background changed by Register

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "./features/userSlice";
 
 const App = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
   return (
     <div className="dark">
-      <button onClick={() => { dispatch(logout()) }}>Logout</button>
+      {/* <button onClick={() => { dispatch(logout()) }}>Logout</button> */}
       <Router>
         <Routes>
           <Route path="/register" element={<Register />} />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      Register
+    <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      {/* Container */}
+      <div className="flex w-[1600px] mx-auto h-full">Register</div>
     </div>
   )
 }
