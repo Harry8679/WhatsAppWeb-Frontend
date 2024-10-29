72a8e931997b3437dd4b607b4aa3d947a9d61034
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 20:47:46 +0100

    Name Validation between 2 and 16 characters

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,8 @@
 import * as Yup from 'yup';
 
 export const signUpSchema = Yup.object({
-    name: Yup.string().required('Full name is required'),
+    name: Yup.string().required('Full name is required')
+        .matches(/^[a-zA-Z_ ]*$/, 'No special characters allowed.')
+        .min(2, 'Name must be between 2 and 16 characters.')
+        .max(16, 'Name must be between 2 and 16 characters.')
 });
