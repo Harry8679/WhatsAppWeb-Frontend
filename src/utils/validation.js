8938bacd2319c5eb6daca9b62e2022ba6eff69b7
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 20:58:57 +0100

    Status Validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,6 +5,11 @@ export const signUpSchema = Yup.object({
         .matches(/^[a-zA-Z_ ]*$/, 'No special characters allowed.')
         .min(2, 'Name must be between 2 and 16 characters.')
         .max(16, 'Name must be between 2 and 16 characters.'),
+
     email: Yup.string().required('Email address is required')
-        .email('Invalid email address')
+        .email('Invalid email address'),
+
+    status: Yup.string().max(64, 'Status must be less than 64 characters.'),
+
+    
 });
