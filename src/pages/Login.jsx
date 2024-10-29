68addc6e25c2bab05c6662fddaf7bac374b5661f
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 19:29:04 +0100

    Login and Register Page updated with the Forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import LoginForm from '../components/auth/LoginForm';
 
 const Login = () => {
   return (
     <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
       {/* Container */}
-      <div className="flex w-[1600px] mx-auto h-full">Login</div>
+      <div className="flex w-[1600px] mx-auto h-full">
+        <LoginForm />
+      </div>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm';
 
 const Register = () => {
   return (
     <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
       {/* Container */}
-      <div className="flex w-[1600px] mx-auto h-full">Register</div>
+      <div className="flex w-[1600px] mx-auto h-full">
+        <RegisterForm />
+      </div>
     </div>
   )
 }
