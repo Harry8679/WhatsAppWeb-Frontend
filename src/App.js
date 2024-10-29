666a99e35db5fa1cb59412fd0e2e192cfbb92e1f
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-29 19:21:35 +0100

    Useless code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "./features/userSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { logout } from "./features/userSlice";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.user);
-  console.log(user);
+  // const dispatch = useDispatch();
+  // const { user } = useSelector((state) => state.user);
+  // console.log(user);
   return (
     <div className="dark">
       {/* <button onClick={() => { dispatch(logout()) }}>Logout</button> */}
