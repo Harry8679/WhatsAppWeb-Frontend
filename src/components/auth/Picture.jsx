6b18911b9078f7b631732288dce683b60157bec1
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-31 00:51:15 +0100

    Upload field

--- src/components/auth/Picture.jsx
+++ src/components/auth/Picture.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const Picture = () => {
+const Picture = ({ readablePicture }) => {
   return (
     <div className='mt-8 content-center dark:text-dark_text_1 space-y-1'>
       <label htmlFor="picture" className='text-sm font-bold tracking-wide'>
         Picture (Optional)
       </label>
+      {readablePicture ? (
+        <div>
+            <img src={readablePicture} alt='picture' />
+        </div>
+      ): (
+        <div className='w-full h-12 dark:bg-dark_bg_3 rounded-md font-bold flex items-center justify-center cursor-pointer'>
+            Upload picture
+        </div>
+      )}
     </div>
   )
 }
