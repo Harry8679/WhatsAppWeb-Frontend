a916867aee4a847b565f573710ad9b39a21b2bb8
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-30 12:44:34 +0100

    Status in the form button and react-spinners installed

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -3,8 +3,10 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { signUpSchema } from '../../utils/validation';
 import AuthInput from './AuthInput';
+import { useSelector } from 'react-redux';
 
 const RegisterForm = () => {
+  const { status } = useSelector((state) => state.user);
   const { register, handleSubmit, watch, formState: { errors } } = useForm({ resolver: yupResolver(signUpSchema) });
   const onSubmit = (data) => console.log(data);
   console.log('values', watch());
@@ -26,7 +28,7 @@ const RegisterForm = () => {
             <AuthInput name='status' type='text' placeholder='Status' register={register} error={errors?.status?.message} />
             <AuthInput name='password' type='password' placeholder='Password' register={register} error={errors?.password?.message} />
             <button type='submit' className='w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none
-            hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300'>Submit</button>
+            hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300'>{status === 'loading' ? 'Loading ...' : 'Sign Up'}</button>
         </form>
       </div>
     </div>
