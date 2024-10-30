3934a7f9ce7a3679fcd865647d5858bc20cd844c
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-30 14:15:46 +0100

    extraReducers

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,4 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const AUTH_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT}/auth`;
 
 const initialState = {
     status: '',
@@ -13,7 +16,14 @@ const initialState = {
     }
 };
 
-
+export const registerUser = createAsyncThunk('auth/register', async (values, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`${AUTH_ENDPOINT}/register`, { ...values });
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.response.data.error.message);
+    }
+});
 
 export const userSlice = createSlice({ 
     name: 'user', 
@@ -31,6 +41,19 @@ export const userSlice = createSlice({
                 token: '',
             };
         }
+    },
+    extraReducers(builder) {
+        builder.addCase(registerUser.pending, (state, action) => {
+            state.status = 'loading';
+        })
+        .addCase(registerUser.fulfilled, (state, action) => {
+            state.status = 'successed';
+            state.user = action.payload.user;
+        })
+        .addCase(registerUser.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload;
+        });
     }
 });
 
