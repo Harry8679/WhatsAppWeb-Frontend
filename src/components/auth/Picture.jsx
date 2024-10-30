5d5e3e538b9afa398bb74c39a4b0eba4a2c3a46a
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-31 00:01:59 +0100

    Picture component updated

--- src/components/auth/Picture.jsx
+++ src/components/auth/Picture.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Picture = () => {
   return (
-    <div>
-      
+    <div className='mt-8 content-center dark:text-dark_text_1 space-y-1'>
+      <label htmlFor="picture" className='text-sm font-bold tracking-wide'>
+        Picture (Optional)
+      </label>
     </div>
   )
 }
