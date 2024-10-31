82f9922e32b574f213a5c5a956fa1a94dd7e2401
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-31 23:28:15 +0100

    HandlePicture

--- src/components/auth/Picture.jsx
+++ src/components/auth/Picture.jsx
@@ -2,6 +2,12 @@ import React, { useRef } from 'react'
 
 const Picture = ({ readablePicture }) => {
   const inputRef = useRef();
+  const handlePicture = (e) => {
+    let pic = e.target.files[0];
+    if (pic.type !== 'image/jpeg' && pic.type !== 'image/png' && pic.type !== 'image/webp') {
+      return;
+    }
+  }
   return (
     <div className='mt-8 content-center dark:text-dark_text_1 space-y-1'>
       <label htmlFor="picture" className='text-sm font-bold tracking-wide'>
@@ -12,7 +18,7 @@ const Picture = ({ readablePicture }) => {
             <img src={readablePicture} alt='about_me' />
         </div>
       ): (
-        <div className='w-full h-12 dark:bg-dark_bg_3 rounded-md font-bold flex items-center justify-center cursor-pointer'>
+        <div className='w-full h-12 dark:bg-dark_bg_3 rounded-md font-bold flex items-center justify-center cursor-pointer' onClick={() => inputRef.current.click()}>
             Upload picture
         </div>
       )}
