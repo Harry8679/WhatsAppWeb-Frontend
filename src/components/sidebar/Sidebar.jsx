4411a3dbda952418861b739bd190c34722d474b2
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 23:38:04 +0100

    SidebarHeader imported in Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SidebarHeader from './header/SidebarHeader';
 
 const Sidebar = () => {
   return (
     <div className='w-[40%] h-full select-none'>
       {/* Sidebar Header */}
-      {/* <SidebarH */}
+      <SidebarHeader />
     </div>
   )
 }
