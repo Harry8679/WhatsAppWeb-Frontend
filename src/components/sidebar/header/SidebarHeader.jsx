0a71ab46e95dca518ca82895bc4547819aac7748
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 23:47:34 +0100

    useSelector

--- src/components/sidebar/header/SidebarHeader.jsx
+++ src/components/sidebar/header/SidebarHeader.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const SidebarHeader = () => {
+  const { user } = useSelector((state) => state.user);
   return (
     <div className='h-[50px] dark:bg-dark_bg_2 flex items-center p16'>
       {/* Container */}
-      <div className="w-full flex items-center justify-between"></div>
+      <div className="w-full flex items-center justify-between">
+        {/* user image */}
+        <button className='btn'>
+            <img src={user.picture} alt="user" />
+        </button>
+      </div>
     </div>
   )
 }
