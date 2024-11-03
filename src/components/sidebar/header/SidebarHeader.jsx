4e2664f4f28b1ff08a3d873f99ee54d8e806fb93
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 23:43:06 +0100

    SidebarHeader updated 1

--- src/components/sidebar/header/SidebarHeader.jsx
+++ src/components/sidebar/header/SidebarHeader.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const SidebarHeader = () => {
   return (
-    <div>
-      SidebarHeader
+    <div className='h-[50px] dark:bg-dark_bg_2 flex items-center p16'>
+      {/* Container */}
+      <div className="w-full flex items-center justify-between"></div>
     </div>
   )
 }
