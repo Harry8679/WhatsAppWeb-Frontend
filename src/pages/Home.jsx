849cbf316b2b671b6237e0d72412c0cc940e2e13
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 23:18:14 +0100

    Home and style updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      {/* container */}
+      <div className="container flex">
+        {/* sidebar */}
+        
+      </div>
     </div>
   )
 }
