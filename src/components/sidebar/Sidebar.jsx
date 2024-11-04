855f4676fa9648a2fe3d0375bfa686714157bb02
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-04 17:27:06 +0100

    Notifications imported

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import SidebarHeader from './header/SidebarHeader';
+import { Notifications } from './notifications';
 
 const Sidebar = () => {
   return (
     <div className='w-[40%] h-full select-none'>
       {/* Sidebar Header */}
       <SidebarHeader />
+      {/* Notifications */}
+      <Notifications />
     </div>
   )
 }
