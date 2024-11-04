6a1d502121b57f820f957319fd8e228fed7c2d06
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-04 18:14:21 +0100

    Notifications updated

--- src/components/sidebar/notifications/Notifications.jsx
+++ src/components/sidebar/notifications/Notifications.jsx
@@ -8,7 +8,9 @@ const Notifications = () => {
       <div className='w-full flex items-center justify-between'>
         {/* Left */}
         <div className="flex items-center gap-x-4">
-            <NotificationIcon className='dark:fill-blue_1' />
+            <div className='cursor-pointer'>
+                <NotificationIcon className='dark:fill-blue_1 cursor-pointer' />
+            </div>
             <div className='flex flex-col'>
                 <span className='textPrimary'>Get notified of new messages</span>
                 <span className='textSecondary mt-0.5 flex items-center gap-0.5'>
@@ -18,7 +20,7 @@ const Notifications = () => {
             </div>
         </div>
         {/* Right */}
-        <div>
+        <div className='cursor-pointer'>
             <CloseIcon className='dark:fill-dark_svg_2' />
         </div>
       </div>
