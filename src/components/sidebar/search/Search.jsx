fd8e29b69f917ec6e0b746251e2da5272544f46d
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-04 22:44:32 +0100

    Icons added

--- src/components/sidebar/search/Search.jsx
+++ src/components/sidebar/search/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { ReturnIcon, SearchIcon } from '../../../svg';
 
-const Search = () => {
+const Search = ({ searchLength }) => {
   const [show, setShow] = useState(false);
   return (
     <div className='h-[49px] py-1.5'>
@@ -9,7 +10,15 @@ const Search = () => {
         {/* Search input container */}
         <div className='flex items-center gap-x-2'>
           <div className='w-full dark:bg-dark_bg_2 rounded-lg pl-2'>
-            
+            {show || searchLength > 0 ? (
+              <span className='w-8 flex items-center justify-center rotateAnimation'>
+                <ReturnIcon className='fill-green_1 w-5' />
+              </span>
+            ) : (
+              <span className='w-8 flex items-center justify-center'>
+                <SearchIcon className='dark:fill-dark_svg_2 w-5' />
+              </span>
+            )}
           </div>
         </div>
       </div>
