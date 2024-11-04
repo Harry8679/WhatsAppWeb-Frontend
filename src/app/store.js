846231404de169fbee209315f12bf0ae2c17c777
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-04 23:24:10 +0100

    chatSlice added in the store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import userSlice from '../features/userSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import createFilter from 'redux-persist-transform-filter';
+import { chatSlice } from '../features/chatSlice';
 
 // Save User Only Filter
 // const saveUserOnlyFilter = createFilter('user', ['user.user']);
@@ -17,7 +18,8 @@ const persistConfig = {
 }
 
 const rootReducer = combineReducers({
-    user: userSlice
+    user: userSlice,
+    chat: chatSlice
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
