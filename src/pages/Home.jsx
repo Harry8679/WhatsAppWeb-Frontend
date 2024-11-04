8835b57cd13c177276ca9d9d6130d278c23bf67b
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-04 16:51:56 +0100

    Style added in Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
       {/* container */}
-      <div className="container flex">
+      <div className="container min-h-screen flex">
         {/* sidebar */}
         <Sidebar />
       </div>
