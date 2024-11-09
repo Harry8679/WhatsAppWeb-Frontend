014d2b77c0b90d55503d857e827c117cf99610ae
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-09 23:48:31 +0100

    Contact updated 2

--- src/components/sidebar/search/Contact.jsx
+++ src/components/sidebar/search/Contact.jsx
@@ -5,8 +5,15 @@ const Contact = () => {
     <div className='list-none h-[72px] hover:dark:bg-dark_bg_2 cursor-pointer dark:text-dark_text_1 px-[10px]'>
       {/* Container */}
       <div className='flex items-center gap-x-3 py-[10px]'>
-        {/* Contact */}
-        
+        {/* Contact infos */}
+        <div className='flex items-center gap-x-3'>
+            {/* Conversation user picture */}
+            <div className='relative min-w-[50px] h-[50px] rounded-full overflow-hidden'>
+                <img src={convo.picture ? convo.picture : 'https://img.freepik.com/vecteurs-libre/cercle-bleu-utilisateur-blanc_78370-4707.jpg'} 
+                    alt={convo.name} className='w-full h-full object-cover' />
+            </div>
+        </div>
+
       </div>
     </div>
   )
