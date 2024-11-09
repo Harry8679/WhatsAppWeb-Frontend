c8b24061d5240b7a3c7aa7e735afb38a3407756e
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-09 14:47:41 +0100

    Search Results updated

--- src/components/sidebar/search/SearchResults.jsx
+++ src/components/sidebar/search/SearchResults.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const SearchResults = () => {
+const SearchResults = ({ searchResults }) => {
   return (
-    <div>
-      Search Results
+    <div className='flex flex-col px-8 pt-8'>
+      <h1 className='font-extralight text-md text-green_2'>Contacts</h1>
+      <span className='w-full mt-4 ml-10 border-b dark:border-b-dark_border_1'></span>
     </div>
   )
 }
