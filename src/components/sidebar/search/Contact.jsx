12828d69a886c4f54a59d6a42bf8b020fbdef3a6
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-09 23:38:10 +0100

    Contact updated

--- src/components/sidebar/search/Contact.jsx
+++ src/components/sidebar/search/Contact.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className='list-none h-[72px] hover:dark:bg-dark_bg_2 cursor-pointer dark:text-dark_text_1 px-[10px]'>
+      {/* Container */}
+      <div className='flex items-center gap-x-3 py-[10px]'>
+        {/* Contact */}
+        
+      </div>
     </div>
   )
 }
