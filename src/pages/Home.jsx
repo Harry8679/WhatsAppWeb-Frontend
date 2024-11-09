afdc0e0bb3215fa49296d6b7d55a68f590baab93
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-10 00:22:23 +0100

    Home updated 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
       {/* container */}
-      <div className="container min-h-screen flex">
+      <div className="container h-screen flex">
         {/* sidebar */}
         <Sidebar />
       </div>
