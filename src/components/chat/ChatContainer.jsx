33722155a548b239a15a0c5a2ee4a465ad722528
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-17 22:52:18 +0100

    Chat Container Updated 1

--- src/components/chat/ChatContainer.jsx
+++ src/components/chat/ChatContainer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ChatContainer = () => {
   return (
-    <div>
-      Chat Container
+    <div className='relative w-full h-full border-1 dark:border-1-dark_border_2 select-none overflow-hidden'>
+      
     </div>
   )
 }
