a9b5517bddb946a67bf664eabdf14d1dced74009
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-17 23:51:18 +0100

    Chat Header

--- src/components/chat/ChatContainer.jsx
+++ src/components/chat/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ChatHeader from './header/ChatHeader';
 
 const ChatContainer = () => {
   return (
     <div className='relative w-full h-full border-1 dark:border-1-dark_border_2 select-none overflow-hidden'>
-      
+      <ChatHeader />
     </div>
   )
 }
