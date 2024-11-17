48de69e85d436f46f208bb6d516d46f513eec3ad
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-17 23:29:51 +0100

    Redux added

--- src/components/chat/header/ChatHeader.jsx
+++ src/components/chat/header/ChatHeader.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ChatHeader = () => {
+  const { activeConversation } = useSelector((state) => state.chat);
+  const { name, picture } = activeConversation;
   return (
     <div>
       Chat Header
